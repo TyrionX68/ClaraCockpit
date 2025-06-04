3a9305ca8ef6fc7e0772d1b079e325ce845d7da1
TyrionX68 <hiss@pro-estate24.de>
2025-06-04 18:41:06 +0200

    router.js

--- router.js
+++ router.js
@@ -1,3 +1,1 @@
-import { createRouter, createWebHistory } from 'vue-router'
-const routes = [ { path: '/', redirect: '/dashboard' }, { path: '/dashboard', component: () => import('./src/components/ClaraDashboard.vue') } ]
-export default createRouter({ history: createWebHistory(), routes })
+import ClaraDashboard from './ClaraDashboard.vue'
