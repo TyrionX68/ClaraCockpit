da0dc924f6d2055a88d3f7bcbf2cb51e527042dd
TyrionX68 <hiss@pro-estate24.de>
2025-06-04 19:38:20 +0200

    router.js

--- router.js
+++ router.js
@@ -1,4 +1,17 @@
-import ClaraDashboard from './
-  ClaraDashboard.vue
+import { createRouter, createWebHistory } from 'vue-router'
+import ClaraDashboard from './ClaraDashboard.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: ClaraDashboard
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 export default router
