5a42f08b18481562d4815366f749ca7753da30c6
TyrionX68 <hiss@pro-estate24.de>
2025-06-27 09:08:56 +0000

    🧪 [TEST-20250627_0915] ManifestPage reaktiviert - Build-Test Schritt 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ModernDashboardPage from './pages/ModernDashboardPage';
+import ManifestPage from './pages/ManifestPage';
 import './styles/modern-design-system.css';
 
 function App() {
@@ -10,11 +11,11 @@ function App() {
         <Routes>
           <Route path="/" element={<ModernDashboardPage />} />
           <Route path="/dashboard" element={<ModernDashboardPage />} />
-          {/* Temporarily disabled routes for build error isolation */}
+          <Route path="/manifest" element={<ManifestPage />} />
+          {/* Still disabled routes for incremental testing */}
           {/* <Route path="/banking" element={<BankingPage />} /> */}
           {/* <Route path="/clara-ki" element={<ClaraKIPage />} /> */}
           {/* <Route path="/ki" element={<ClaraKIPage />} /> */}
-          {/* <Route path="/manifest" element={<ManifestPage />} /> */}
           {/* <Route path="/kommunikation" element={<KommunikationPage />} /> */}
           <Route path="*" element={<ModernDashboardPage />} />
         </Routes>
