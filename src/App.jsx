3af219ad5617d9826a551353ddcb3a7dcb6737bf
TyrionX68 <hiss@pro-estate24.de>
2025-06-27 09:03:32 +0000

    🚑 [ISOLATION-20250627_0910] Alle neuen Routen temporär deaktiviert - Build-Fehler isolieren

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ModernDashboardPage from './pages/ModernDashboardPage';
-import BankingPage from './pages/BankingPage';
-import ClaraKIPage from './pages/ClaraKIPage';
-import ManifestPage from './pages/ManifestPage';
-import KommunikationPage from './pages/KommunikationPage';
 import './styles/modern-design-system.css';
 
 function App() {
@@ -14,11 +10,12 @@ function App() {
         <Routes>
           <Route path="/" element={<ModernDashboardPage />} />
           <Route path="/dashboard" element={<ModernDashboardPage />} />
-          <Route path="/banking" element={<BankingPage />} />
-          <Route path="/clara-ki" element={<ClaraKIPage />} />
-          <Route path="/ki" element={<ClaraKIPage />} />
-          <Route path="/manifest" element={<ManifestPage />} />
-          <Route path="/kommunikation" element={<KommunikationPage />} />
+          {/* Temporarily disabled routes for build error isolation */}
+          {/* <Route path="/banking" element={<BankingPage />} /> */}
+          {/* <Route path="/clara-ki" element={<ClaraKIPage />} /> */}
+          {/* <Route path="/ki" element={<ClaraKIPage />} /> */}
+          {/* <Route path="/manifest" element={<ManifestPage />} /> */}
+          {/* <Route path="/kommunikation" element={<KommunikationPage />} /> */}
           <Route path="*" element={<ModernDashboardPage />} />
         </Routes>
       </div>
